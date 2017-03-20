841c243348e679a716903e98489259b2018f5fe4
李主贤 <lizhuxian020@pingan.com.cn>
2017-03-20 11:11:45 +0800

    init

---
+++ HMSugarCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMSugarCode
+//
+//  Created by lzx on 17/3/20.
+//  Copyright © 2017年 lzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
